8289a1e8e8a5e8684f64313175d2b623d6031a69
TXG0Fk3 <leovertonx12@outlook.com>
2024-09-20 10:08:27 -0300

    ChAAA

--- YT Downloader/MainWindow.xaml.cs
+++ YT Downloader/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Runtime.InteropServices;
 using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Animation;
+using Windows.ApplicationModel.Core;
 using Windows.Graphics;
 using Windows.UI;
 using Windows.UI.ViewManagement;
@@ -22,6 +24,11 @@ namespace YT_Downloader
             ExtendsContentIntoTitleBar = true; // TitleBar infinito
             AppWindow.Title = "YT Downloader"; // Título
             AppWindow.SetIcon(@"Assets\AppIcon.ico"); // Ícone
+            (AppWindow.Presenter as OverlappedPresenter).IsResizable = false; // Torna a janela não redimensionável
+
+            // Desativa o botão de maximizar
+            IntPtr hwnd = WindowNative.GetWindowHandle(this);
+            _ = SetWindowLong(hwnd, -16, GetWindowLong(hwnd, -16) & ~0x00010000);
 
             // Aplica o tema
             ApplyTheme(Enum.TryParse<ElementTheme>(App.appSettings.Theme, out var parsedTheme)
@@ -36,23 +43,18 @@ namespace YT_Downloader
         // Ajusta resolução do app de acordo com a DPI (escala) do monitor
         private void AdjustWindowSizeForDpi(object sender, WindowActivatedEventArgs args)
         {
-            int dpi = GetDpiForWindow(WinRT.Interop.WindowNative.GetWindowHandle(this));
+            int dpi = GetDpiForWindow(WindowNative.GetWindowHandle(this));
             double scaleFactor = dpi / 96.0; // 96 é o DPI padrão de 100%
 
-            // Ajusta de acordo com a escala
-            double Width = 660 * scaleFactor;
-            double Height = 410 * scaleFactor;
-            AppWindow.Resize(new SizeInt32((int)Width, (int)Height));
+            // Ajusta de acordo com a escala    
+            AppWindow.Resize(new SizeInt32((int)(660 * scaleFactor), (int)(410 * scaleFactor)));
         }
 
         // Aplica o tema
         public void ApplyTheme(ElementTheme theme)
         {
-            // Aplica o tema ao rootElement (content root da janela)
-            rootElement.RequestedTheme = theme;
-
-            // Customização da TitleBar diretamente no WinUI 3
-            var titleBar = AppWindow.TitleBar;
+            rootElement.RequestedTheme = theme; // Aplica o tema ao rootElement (content root da janela)
+            var titleBar = AppWindow.TitleBar; // Customização da TitleBar diretamente no WinUI 3
 
             // Determina o tema a ser aplicado
             if (theme == ElementTheme.Default)
@@ -62,21 +64,13 @@ namespace YT_Downloader
                     : ElementTheme.Light;
             }
 
+            Color buttonHoverBackgroundColor = theme == ElementTheme.Dark ? Color.FromArgb(255, 61, 61, 61) : Colors.LightGray;
+            Color foregroundColor = theme == ElementTheme.Dark ? Colors.White : Colors.Black;
 
-            if (theme == ElementTheme.Dark)
-            {
-                titleBar.ButtonHoverBackgroundColor = Color.FromArgb(255, 61, 61, 61);
-                titleBar.ForegroundColor = Colors.White;
-                titleBar.ButtonForegroundColor = Colors.White;
-                titleBar.ButtonHoverForegroundColor = Colors.White;
-            }
-            else if (theme == ElementTheme.Light)
-            { 
-                titleBar.ButtonHoverBackgroundColor = Colors.LightGray;
-                titleBar.ForegroundColor = Colors.Black;
-                titleBar.ButtonForegroundColor = Colors.Black;
-                titleBar.ButtonHoverForegroundColor = Colors.Black;
-            }
+            titleBar.ButtonHoverBackgroundColor = buttonHoverBackgroundColor;
+            titleBar.ForegroundColor = foregroundColor;
+            titleBar.ButtonForegroundColor = foregroundColor;
+            titleBar.ButtonHoverForegroundColor = foregroundColor;
         }
 
 
@@ -108,5 +102,11 @@ namespace YT_Downloader
 
         [DllImport("user32.dll")]
         private static extern int GetDpiForWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
     }
 }
