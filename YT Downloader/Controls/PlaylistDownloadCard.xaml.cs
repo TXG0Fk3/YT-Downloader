abe61f31210cf7f800f5b08c91b58026997d0a86
TXG0Fk3 <leovertonx12@outlook.com>
2025-01-02 23:23:19 -0300

    a

---
+++ YT Downloader/Controls/PlaylistDownloadCard.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace YT_Downloader.Controls
+{
+    public sealed partial class PlaylistDownloadCard : UserControl
+    {
+        public PlaylistDownloadCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
