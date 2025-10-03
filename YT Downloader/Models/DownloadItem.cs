075951e94bcfff38b80dd56490a8ef5267af9534
TXG0Fk3 <leovertonx12@outlook.com>
2025-10-03 17:29:01 -0300

    feat: add DownloadItem model

---
+++ YT Downloader/Models/DownloadItem.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading.Tasks;
+using YT_Downloader.Enums;
+
+namespace YT_Downloader.Models
+{
+    public class DownloadItem
+    {
+        public string VideoId { get; set; }
+        public string OutputPath { get; set; }
+        public double Progress { get; private set; }
+        public DownloadStatus Status { get; set; } = DownloadStatus.Pending;
+        public Exception Error { get; set; }
+        public Task DownloadTask { get; set; }
+
+        public event Action<DownloadItem> Completed;
+        public event Action<DownloadItem> ProgressChanged;
+
+        internal void OnCompleted()
+        {
+            Completed?.Invoke(this);
+        }
+
+        public void UpdateProgress(double value)
+        {
+            Progress = value;
+            ProgressChanged?.Invoke(this);
+        }
+
+        public void MarkAsDownloading() => Status = DownloadStatus.Downloading;
+        public void MarkAsCompleted() => Status = DownloadStatus.Completed;
+        public void MarkAsCancelled() => Status = DownloadStatus.Cancelled;
+        public void MarkAsError(Exception ex)
+        {
+            Status = DownloadStatus.Error;
+            Error = ex;
+        }
+    }
+}
