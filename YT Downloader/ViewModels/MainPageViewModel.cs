1a2cfe5d9980f7438fbae217aa016349e193f361
TXG0Fk3 <leovertonx12@outlook.com>
2025-10-27 17:14:26 -0300

    feat: add initial MainPageViewModel

---
+++ YT Downloader/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,25 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using YT_Downloader.Models;
+using YT_Downloader.Services;
+
+namespace YT_Downloader.ViewModels
+{
+    public partial class MainPageViewModel : ObservableObject
+    {
+        private readonly DownloadsService _downloadsService;
+        private readonly DialogService _dialogService;
+
+        public ObservableCollection<IDownloadable> Downloads { get; private set; } = new();
+
+        public bool IsDownloadItemsEmpty => Downloads.Count == 0;
+
+        public MainPageViewModel(DownloadsService downloadsService, DialogService dialogService)
+        {
+            _downloadsService = downloadsService;
+            _dialogService = dialogService;
+
+            Downloads.CollectionChanged += (s, e) => OnPropertyChanged(nameof(IsDownloadItemsEmpty));
+        }
+    }
+}
