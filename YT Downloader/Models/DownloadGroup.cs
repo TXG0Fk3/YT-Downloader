48fe9e742ba607dac9a36f33625880757a58e5bb
TXG0Fk3 <leovertonx12@outlook.com>
2025-11-01 19:32:07 -0300

    refactor: add notification logic and more properties

--- YT Downloader/Models/DownloadGroup.cs
+++ YT Downloader/Models/DownloadGroup.cs
@@ -1,16 +1,22 @@
-using System.Collections.Generic;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using YT_Downloader.Enums;
 
 namespace YT_Downloader.Models
 {
-    public class DownloadGroup : IDownloadable
+    public class DownloadGroup : ObservableObject, IDownloadable
     {
         public required string PlaylistId { get; set; }
         public required string Title { get; set; }
         public required string Author { get; set; }
+        public required string Url { get; set; }
+        public required DownloadType Type { get; set; }
         public required string Quality { get; set; }
-        public List<DownloadItem> Items { get; set; } = new();
+        public required string OutputPath { get; set; }
+        public ObservableCollection<DownloadItem> Items { get; } = new();
 
         public double Progress => Items.Count == 0 ? 0 : Items.Average(i => i.Progress);
 
@@ -20,5 +26,36 @@ namespace YT_Downloader.Models
                 : Items.Any(i => i.Status == DownloadStatus.Downloading)
                     ? DownloadStatus.Downloading
                     : DownloadStatus.Pending;
+
+        private void OnItemsChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null)
+            {
+                foreach (DownloadItem item in e.NewItems)
+                {
+                    item.PropertyChanged += OnItemPropertyChanged;
+                }
+            }
+
+            if (e.OldItems != null)
+            {
+                foreach (DownloadItem item in e.OldItems)
+                {
+                    item.PropertyChanged -= OnItemPropertyChanged;
+                }
+            }
+
+            OnPropertyChanged(nameof(Progress));
+            OnPropertyChanged(nameof(Status));
+        }
+
+        private void OnItemPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(DownloadItem.Progress))
+                OnPropertyChanged(nameof(Progress));
+
+            if (e.PropertyName == nameof(DownloadItem.Status))
+                OnPropertyChanged(nameof(Status));
+        }
     }
 }
