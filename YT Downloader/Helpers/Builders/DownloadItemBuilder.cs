2a7424a1dff61728541fe8e8ea2e7b59d2157688
TXG0Fk3 <leovertonx12@outlook.com>
2025-11-03 18:34:13 -0300

    feat: add DownloadItemBuilder

---
+++ YT Downloader/Helpers/Builders/DownloadItemBuilder.cs
@@ -0,0 +1,46 @@
+using YT_Downloader.Enums;
+using YT_Downloader.Models;
+using YT_Downloader.Models.Info;
+
+namespace YT_Downloader.Helpers.Builders
+{
+    public class DownloadItemBuilder
+    {
+        private readonly DownloadItem _item = new();
+
+        public DownloadItemBuilder FromVideoInfo(VideoInfo video)
+        {
+            _item.VideoId = video.Id;
+            _item.Url = video.Url;
+            _item.Title = video.Title;
+            _item.Author = video.Author;
+            _item.ThumbnailUrl = video.ThumbnailUrl;
+            return this;
+        }
+
+        public DownloadItemBuilder WithOutputPath(string path)
+        {
+            _item.OutputPath = path;
+            return this;
+        }
+
+        public DownloadItemBuilder AsVideo(string quality, StreamOption videoStream, StreamOption audioStream)
+        {
+            _item.Type = DownloadType.Video;
+            _item.Quality = quality;
+            _item.VideoStreamOption = videoStream;
+            _item.AudioStreamOption = audioStream;
+            return this;
+        }
+
+        public DownloadItemBuilder AsAudio(StreamOption audioStream)
+        {
+            _item.Type = DownloadType.Audio;
+            _item.Quality = "Best";
+            _item.AudioStreamOption = audioStream;
+            return this;
+        }
+
+        public DownloadItem Build() => _item;
+    }
+}
