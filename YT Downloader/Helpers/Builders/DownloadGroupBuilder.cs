4752e9ce3838a61cf602c75027368b20d9d4b328
TXG0Fk3 <leovertonx12@outlook.com>
2025-11-03 18:34:22 -0300

    feat: add DownloadGroupBuilder

---
+++ YT Downloader/Helpers/Builders/DownloadGroupBuilder.cs
@@ -0,0 +1,36 @@
+using YT_Downloader.Enums;
+using YT_Downloader.Models;
+using YT_Downloader.Models.Info;
+
+namespace YT_Downloader.Helpers.Builders
+{
+    public class DownloadGroupBuilder
+    {
+        private readonly DownloadGroup _group = new();
+
+        public DownloadGroupBuilder FromPlaylistInfo(PlaylistInfo playlist)
+        {
+            _group.PlaylistId = playlist.Id;
+            _group.Url = playlist.Url;
+            _group.Title = playlist.Title;
+            _group.Author = playlist.Author;
+            return this;
+        }
+
+        public DownloadGroupBuilder WithOutputPath(string outputPath)
+        {
+            _group.OutputPath = outputPath;
+            return this;
+        }
+
+        public DownloadGroupBuilder WithFormat(DownloadType type, string quality)
+        {
+            _group.Type = type;
+            _group.Quality = quality;
+            return this;
+        }
+
+        public DownloadGroup Build() => _group;
+    }
+
+}
