429be33c5bbcf60a5f49647ea5ae94b404b1a299
TXG0Fk3 <leovertonx12@outlook.com>
2025-11-06 23:14:52 -0300

    feat: add SettingsDialog

---
+++ YT Downloader/Views/Dialogs/SettingsDialog.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace YT_Downloader.Views.Dialogs
+{
+    public sealed partial class SettingsDialog : ContentDialog
+    {
+        public SettingsDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
