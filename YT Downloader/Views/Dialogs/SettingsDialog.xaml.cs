759a61ce121c53b1d182c246bf42ead8c5950762
TXG0Fk3 <leovertonx12@outlook.com>
2025-11-07 01:26:10 -0300

    fix: make SettingsDialog react when the theme changes.

--- YT Downloader/Views/Dialogs/SettingsDialog.xaml.cs
+++ YT Downloader/Views/Dialogs/SettingsDialog.xaml.cs
@@ -1,4 +1,6 @@
 using Microsoft.UI.Xaml.Controls;
+using YT_Downloader.Enums;
+using YT_Downloader.Helpers.UI;
 using YT_Downloader.ViewModels.Dialogs;
 
 namespace YT_Downloader.Views.Dialogs
@@ -14,7 +16,10 @@ namespace YT_Downloader.Views.Dialogs
         private void RadioButtons_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (DataContext is SettingsDialogViewModel VM)
+            {
                 VM.SelectThemeCommand.Execute(null);
+                RequestedTheme = ThemeHelper.ConvertThemeOptionToElementTheme(VM.SelectedThemeOption);
+            }
         }
 
         private void ToggleSwitch_Toggled(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
