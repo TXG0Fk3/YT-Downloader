ac54b7ba96bd928b095f931c9e15ad2291658b24
TXG0Fk3 <leovertonx12@outlook.com>
2025-11-06 21:51:19 -0300

    feat: add HelpDialog

---
+++ YT Downloader/Views/Dialogs/HelpDialog.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace YT_Downloader.Views.Dialogs
+{
+    public sealed partial class HelpDialog : ContentDialog
+    {
+        public HelpDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
